641a1a8421970fcffc9f84bf82fbe4b691e0084a
Seongryeol <102934991+tjdfuf@users.noreply.github.com>
2022-06-03 09:43:54 +0900

    Add files via upload

---
+++ miniProject3/health/Test.java
@@ -0,0 +1,24 @@
+package com.kh.miniProject3.health;
+
+import com.kh.miniProject3.health.controller.HealthMemberController;
+
+import java.util.Arrays;
+
+public class Test {
+    public static void main(String[] args) {
+
+        HealthMemberController mc = new HealthMemberController();
+
+
+//        System.out.println(mc.findIndexById("A02"));
+//        System.out.println(mc.searchId("A02"));
+//        System.out.println(mc.delete("A01"));
+//        System.out.println(mc.delete("A02"));
+//        System.out.println(mc.existMemberNum());
+        System.out.println(mc.lockerList());
+
+
+    }
+
+
+}
---
+++ miniProject3/health/model/Locker.java
@@ -0,0 +1,9 @@
+package com.kh.miniProject3.health.model;
+
+public class Locker {
+
+    private int lockerNum;
+    private String inMemberId;
+
+
+}
